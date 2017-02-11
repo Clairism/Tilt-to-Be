4b6c52de398736bf304ccdee35d0466c3fbeda36
NicoShi <xin@animateception.com>
2017-02-11 15:16:31 -0500

    First commit

---
+++ Assets/ChooseLife.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseLife : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision other) {
+		Debug.Log ("ENTEREED");
+		if (other.gameObject.name == "richest") {
+			//Debug.Log ("richest");
+		}
+
+		if (other.gameObject.name == "rich") {
+		}
+
+		if (other.gameObject.name == "ok") {
+		}
+
+		if (other.gameObject.name == "poor") {
+		}
+
+		if (other.gameObject.name == "poorest") {
+		}
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		//Debug.Log ("enter!");
+		if (other.gameObject.name == "player") {
+			//Debug.Log ("player enter");
+			ObjectManager.s.destiny = gameObject.name.ToString ();
+			Debug.Log (ObjectManager.s.destiny);
+		}
+
+	}
+}
---
+++ Assets/ObjectManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManager : MonoBehaviour {
+	//Player's destiny
+	public string destiny = "";
+
+
+
+	private static ObjectManager singleton;
+	public static ObjectManager s { get { return singleton; } }
+
+	protected void Awake(){
+		singleton = this;
+	}
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
